82ec51aaf7834cace1aad3cdac3d498d030c53d4
titulebolide <seneztit@gmail.com>
2022-03-09 11:27:24 +0100

    add compatibility for rverse poxies

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import flask
+from flask import Flask, request
 import os
 import config
 from apprise import Apprise
@@ -6,14 +6,17 @@ from apprise import Apprise
 apprise = Apprise()
 apprise.add(config.TGRAM_URL, tag="tgram")
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 if not os.path.isfile('static/' + config.IMAGE_NAME):
     os.system(f"cd static && curl -o {config.IMAGE_NAME} https://upload.wikimedia.org/wikipedia/commons/4/49/Carte_identit%C3%A9_%C3%A9lectronique_fran%C3%A7aise_%282021%2C_recto%29.png")
 
 @app.route(config.URL)
 def index():
-    msg = "New request from " + flask.request.remote_addr
+    ip = request.environ.get('HTTP_X_FORWARDED_FOR')
+    if ip is None:
+        ip = request.environ['REMOTE_ADDR']
+    msg = "New request from " + ip
     os.system(
         f"apprise -b '{msg}' {config.TGRAM_URL} &"
     )
